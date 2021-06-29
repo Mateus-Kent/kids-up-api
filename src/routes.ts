1c73441ca3234763be47a40d4ab0a0817e1238ce
Mateus-Kent <benriba2003@gmail.com>
2021-06-29 18:09:53 -0300

    adiciona rota de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import ParentController from "./controllers/ParentController";
+import AuthController from "./controllers/AuthController";
 
 const router = Router();
 
 router.post('/parents', (req, res) => ParentController.store(req, res));
+
+router.post('/auth', (req, res) => AuthController.authenticate(req, res));
 
 export default router;
