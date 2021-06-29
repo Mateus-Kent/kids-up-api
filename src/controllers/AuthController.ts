52b5a11ecf84303840a2aa5a31d5dc3f5328d87d
Mateus-Kent <benriba2003@gmail.com>
2021-06-29 18:07:32 -0300

    feat: adiciona controle de autenticação

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import Parent from '../models/Parent';
+
+class AuthController{
+async authenticate(req: Request, res: Response){
+  const repository = getRepository(Parent);
+  const { username, email, phone_number, password, profile_photo } = req.body;
+
+  const parent = await repository.findOne({ where: { email }});
+   
+  if(!parent) {
+    return res.status(401).json({ message: "This user don't exists" });
+  }
+
+  const isValidPassword = await bcrypt.compare(password, parent.password);
+
+  if(!isValidPassword) {
+    return res.status(401).json({ message: "This password don't exists" });
+  }
+ 
+  const token = jwt.sign({ id: parent.id}, 'secret', {});
+
+  return res.json({
+    parent,
+    token,
+  });
+}
+}
+export default new AuthController();
