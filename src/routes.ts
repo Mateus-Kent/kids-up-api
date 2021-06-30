9648e28400f9ed132d8488f90df48fac7cdd2148
Mateus-Kent <benriba2003@gmail.com>
2021-06-30 13:26:06 -0300

    feat: adiciona rota do midleware de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import Middleware from "./middlewares/AuthMiddleware"
 import ParentController from "./controllers/ParentController";
 import AuthController from "./controllers/AuthController";
 
@@ -8,5 +9,7 @@ const router = Router();
 router.post('/parents', (req, res) => ParentController.store(req, res));
 
 router.post('/auth', (req, res) => AuthController.authenticate(req, res));
+
+router.get('/parents', Middleware, (req, res) =>  ParentController.index(req, res));
 
 export default router;
