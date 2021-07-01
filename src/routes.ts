3e47833c81a55644cc49aba494e64f7b2d748c03
Mateus-Kent <benriba2003@gmail.com>
2021-07-01 19:19:26 -0300

    feat: adiciona validação com celebrate

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { celebrate, Joi, Segments, errors } from 'celebrate';
 
 import Middleware from "./middlewares/AuthMiddleware"
 import ParentController from "./controllers/ParentController";
@@ -6,10 +7,24 @@ import AuthController from "./controllers/AuthController";
 
 const router = Router();
 
-router.post('/parents', (req, res) => ParentController.store(req, res));
+router.post('/parents',celebrate({
+ [Segments.BODY]: Joi.object().keys({
+   username: Joi.string().required(),
+   email: Joi.string().email(),
+   password: Joi.string().min(4).max(22).required(),
+   profile_photo: Joi.string().required(),
+ }),
+ [Segments.QUERY]: {
+   token: Joi.string().token().required()
+ }
+}), (req, res) => ParentController.store(req, res));
+
+
 
 router.post('/auth', (req, res) => AuthController.authenticate(req, res));
 
 router.get('/parents', Middleware, (req, res) =>  ParentController.index(req, res));
+
+router.use(errors());
 
 export default router;
