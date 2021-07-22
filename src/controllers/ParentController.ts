8f233515965ddfc5ed1c694585af368b123bef8d
Mateus-Kent <benriba2003@gmail.com>
2021-07-22 16:09:38 -0300

    feat: adiciona função de update

--- src/controllers/ParentController.ts
+++ src/controllers/ParentController.ts
@@ -3,51 +3,34 @@ import { getRepository } from 'typeorm'
 
 import Parent from '../models/Parent'
 
-class ParentController{
-  
-  
- async Update(req: Request , res: Response) {
+class ParentController {
 
-  const { email, username, phone_number } = req.body
 
-  const repository = await getRepository(Parent)
+  async Update(req: Request, res: Response) {
 
-  const parent = await repository.findOne({email})
+    const { email } = req.body
 
-  if(!email){
-    return res.status(401).json({message: 'You have to be logged to update the username'})
-  }
+    const toUpdate = req.body;
 
-  if(!parent){
-    return res.status(401).json({message: 'This user don\'t exist'})
-  }
+    delete toUpdate.email;
 
-  const updatedParent = await repository.update(parent, { username });
-
-    return res.json(updatedParent);
-  }
-
-  async UpdatePhoneNumber(req: Request , res: Response) {
-
-    const { email, phone_number } = req.body
-  
     const repository = await getRepository(Parent)
-  
-    const parent = await repository.findOne({email})
-  
-    if(!email){
-      return res.status(401).json({message: 'You have to be logged to update the username'})
-    }
-  
-    if(!parent){
-      return res.status(401).json({message: 'This user don\'t exist'})
-    }
-  
-    const updatedParent = await repository.update(parent, { phone_number });
-  
-    return res.json(updatedParent);
-  }
-}
 
+    if (!email) {
+      return res.status(401).json({ message: 'You have to be logged to update the username' })
+    }
+
+    const parent = await repository.findOne({ email })
+
+    if (!parent) {
+      return res.status(401).json({ message: 'This user don\'t exist' })
+    }
+
+    await repository.update(parent, toUpdate);
+
+    return res.json({ ...parent, ...toUpdate });
+  }
+
+}
 
 export default new ParentController();
