b853df1bcbe7b3b3c62177d0f88d9ef6f1a34d84
mateus-kent <mateusquent2003@gmail.com>
2022-01-12 18:39:22 -0300

    refactor: uses a different formatting

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,44 +1,43 @@
-import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import { Request, Response } from "express"
+import { getRepository } from "typeorm"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
-import User from "../models/User";
+import User from "../Models/User"
 
 class AuthController {
-
   async authenticate(req: Request, res: Response) {
-    const repository = getRepository(User);
-    const { email, password } = req.body;
+    const repository = getRepository(User)
+    const { email, password } = req.body
 
-    const user = await repository.findOne({ where: { email } });
+    const user = await repository.findOne({ where: { email } })
 
     if (!user) {
-      return res.status(401).json({ message: "This user don't exists" });
+      return res.status(401).json({ message: "This user don't exists" })
     }
 
-    const isValidPassword = await bcrypt.compare(password, user.password);
+    const isValidPassword = await bcrypt.compare(password, user.password)
 
     if (!isValidPassword) {
-      return res.status(401).json({ message: "This password is not the same" });
+      return res.status(401).json({ message: "This password is not the same" })
     }
 
-    const token = jwt.sign({ ...user }, "secret", {});
+    const token = jwt.sign({ ...user }, "secret", {})
 
     return res.json({
       user,
       token,
-    });
+    })
   }
 
   async register(req: Request, res: Response) {
-    const repository = getRepository(User);
-    const { username, email, phone_number, password, profile_photo } = req.body;
+    const repository = getRepository(User)
+    const { username, email, phone_number, password, profile_photo } = req.body
 
-    const userExists = await repository.findOne({ where: { email } });
+    const userExists = await repository.findOne({ where: { email } })
 
     if (userExists) {
-      return res.status(409).json({ message: "This user already exists" });
+      return res.status(409).json({ message: "This user already exists" })
     }
 
     const user = await repository.create({
@@ -47,17 +46,15 @@ class AuthController {
       phone_number,
       password,
       profile_photo,
-      isCaregiver: false
-    });
-    await repository.save(user);
+      isCaregiver: false,
+    })
+    await repository.save(user)
 
+    const token = jwt.sign({ ...user }, "secret", {})
 
-    const token = jwt.sign({ ...user }, "secret", {});
+    console.log(req.headers)
 
-
-    console.log(req.headers);
-
-    return res.json({ user, token });
+    return res.json({ user, token })
   }
 }
-export default new AuthController();
+export default new AuthController()
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,37 +1,36 @@
-import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { Request, Response } from "express"
+import { getRepository } from "typeorm"
 
-import User from "../models/User";
+import User from "../Models/User"
 
 class UserController {
   async Update(req: Request, res: Response) {
-    const { email } = req.user;
+    const { email } = req.user
 
-    console.log(req.user);
+    console.log(req.user)
 
-    const toUpdate = req.body;
+    const toUpdate = req.body
 
-    const repository = await getRepository(User);
+    const repository = await getRepository(User)
 
     if (!email) {
-      return res.status(401).json({ message: "The field email is necessary" });
+      return res.status(401).json({ message: "The field email is necessary" })
     }
 
     console.log("passou email")
 
-    const user = await repository.findOne({ email });
+    const user = await repository.findOne({ email })
 
     if (!user) {
-      return res.status(401).json({ message: "This user don't exist" });
+      return res.status(401).json({ message: "This user don't exist" })
     }
-
 
     console.log(toUpdate)
 
-    await repository.update(user, toUpdate);
+    await repository.update(user, toUpdate)
 
-    return res.json({ ...user, ...toUpdate });
+    return res.json({ ...user, ...toUpdate })
   }
 }
 
-export default new UserController();
+export default new UserController()
